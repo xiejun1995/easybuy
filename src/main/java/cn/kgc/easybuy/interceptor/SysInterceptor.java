06da67e50ac8728398725a9254f8490f56b86df9
xiejunjun <18045972801@163.com>
2019-12-28 16:02:28 +0800

    maven ssm

---
+++ src/main/java/cn/kgc/easybuy/interceptor/SysInterceptor.java
@@ -0,0 +1,11 @@
+package cn.kgc.easybuy.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * @author 谢军军
+ * @Email 18045972801@163.com
+ * @date 2019/12/28 11:13
+ */
+public class SysInterceptor implements HandlerInterceptor {
+}
---
+++ src/main/java/cn/kgc/easybuy/mapper/OrderMapper.java
@@ -0,0 +1,9 @@
+package cn.kgc.easybuy.mapper;
+
+/**
+ * @author 谢军军
+ * @Email 18045972801@163.com
+ * @date 2019/12/28 10:51
+ */
+public class OrderMapper {
+}
